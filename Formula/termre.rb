cd5d1f9107589009819edde37fbf673f090a70f7
golonzovsky <alex.golonzovsky@gmail.com>
2026-08-20 00:05:39 +0200

    termre 0.6.2

--- Formula/termre.rb
+++ Formula/termre.rb
@@ -1,20 +1,20 @@
 class Termre < Formula
   desc "TERMinal REader - PDF/book reader for terminals using kitty graphics"
   homepage "https://term.re"
-  version "0.6.1"
+  version "0.6.2"
   license "AGPL-3.0-or-later"
 
   on_macos do
     on_arm do
       url "https://github.com/golonzovsky/termre/releases/download/v#{version}/re-v#{version}-macos-arm64.tar.gz"
-      sha256 "08826413751404511a64b4e3f82bfb6ac62ca26feef118e7dfbd40490d914499"
+      sha256 "483c4154fa102142c7cb8d170672ea4cbf14ff5a303f3b189302b4eb9e83647f"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/golonzovsky/termre/releases/download/v#{version}/re-v#{version}-linux-x86_64.tar.gz"
-      sha256 "ef5bbe7ffb9cf543ca0819ec493577a8f60e450604c07711b61eff6470cae181"
+      sha256 "e5ccd50b1e7d1fa0637dbf23de9071f871fc867b6884a66b79f93e54ced9a88a"
     end
   end
 
